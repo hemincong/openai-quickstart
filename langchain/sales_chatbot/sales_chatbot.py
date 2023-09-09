dfcccf729e988adfff18c157b18fa2e9521b9ac3
mincong <hemincong@gmail.com>
2023-09-10 02:15:50 +0800

    完成作业2
    扩展销售机器人（sales_chatbot）项目，应用于不同的销售场景，如：电器、家装、教育等。

--- langchain/sales_chatbot/sales_chatbot.py
+++ langchain/sales_chatbot/sales_chatbot.py
@@ -1,8 +1,5 @@
 import gradio as gr
-import random
-import time
 
-from typing import List
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -10,18 +7,37 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 
 
-def initialize_sales_bot(vector_store_dir: str="real_estates_sale"):
+def initialize_vector_store_dir(source_name):
+    from langchain.text_splitter import CharacterTextSplitter
+
+    with open(source_name + '.txt') as f:
+        sales_data = f.read()
+
+        text_splitter = CharacterTextSplitter(
+            separator=r'\d+\.',
+            chunk_size=100,
+            chunk_overlap=0,
+            length_function=len,
+            is_separator_regex=True,
+        )
+        docs = text_splitter.create_documents([sales_data])
+        db = FAISS.from_documents(docs, OpenAIEmbeddings())
+        db.save_local(source_name)
+
+
+def initialize_sales_bot(vector_store_dir: str = "real_estates_sale"):
     db = FAISS.load_local(vector_store_dir, OpenAIEmbeddings())
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    
-    global SALES_BOT    
+
+    global SALES_BOT
     SALES_BOT = RetrievalQA.from_chain_type(llm,
-                                           retriever=db.as_retriever(search_type="similarity_score_threshold",
-                                                                     search_kwargs={"score_threshold": 0.8}))
+                                            retriever=db.as_retriever(search_type="similarity_score_threshold",
+                                                                      search_kwargs={"score_threshold": 0.8}))
     # 返回向量数据库的检索结果
     SALES_BOT.return_source_documents = True
 
     return SALES_BOT
+
 
 def sales_chat(message, history):
     print(f"[message]{message}")
@@ -39,12 +55,12 @@ def sales_chat(message, history):
     # 否则输出套路话术
     else:
         return "这个问题我要问问领导"
-    
 
-def launch_gradio():
+
+def launch_gradio(title=""):
     demo = gr.ChatInterface(
         fn=sales_chat,
-        title="房产销售",
+        title=title,
         # retry_btn=None,
         # undo_btn=None,
         chatbot=gr.Chatbot(height=600),
@@ -52,8 +68,29 @@ def launch_gradio():
 
     demo.launch(share=True, server_name="0.0.0.0")
 
+
+rot_type = {
+    "地产": ("地产销售", "real_estate_sales_data"),
+    "教育": ("教育销售", "education_sales_data"),
+    "电器": ("电器销售", "electrical_appliance_sales_data"),
+    "家装": ("家装销售", "home_improvement_sales_data"),
+}
+
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog='sales_chatbot',
+        description='sale chatbot',
+        epilog='Text at the bottom of help')
+    parser.add_argument('--type')
+    args = parser.parse_args()
+    t = args.type
+
+    print("当前聊天机器人类型：{t}".format(t=t))
+
+    initialize_vector_store_dir(rot_type[t][1])
     # 初始化房产销售机器人
-    initialize_sales_bot()
+    initialize_sales_bot(rot_type[t][1])
     # 启动 Gradio 服务
-    launch_gradio()
+    launch_gradio(rot_type[t][0])
